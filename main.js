cd8f6638cdaf81d1c96cff7dd0af59338b9967cb
Doryan <doryan.chaigneau@hotmail.fr>
2021-02-01 11:23:38 +0100

    Third commit

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 const APIKEY = '366e5f70d9e70e45364730cb9a4f0221';
 
-/*Appel a l'API openWeather avec ville en parametre de fonction*/
+// Appel a l'API openWeather avec ville en parametre de fonction
 let apiCall = function (city) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=fr
-`;
-
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=fr`;
+  
   fetch(url)
-    .then((response) =>
-      response.json().then((data) => {
-        console.log(data);
-        document.querySelector('#ville').innerHTML = data.name;
-        document.querySelector('#temperature').innerHTML =
-          "<i class='fas fa-thermometer-half'></i>" + data.main.temp + '°';
-        document.querySelector('#humidite').innerHTML =
-          "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
-        document.querySelector('#vent').innerHTML =
-          "<i class='fas fa-wind'></i>" + data.wind.speed + 'km/h';
-        document.querySelector(
-          'footer'
-        ).innerHTML = `<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png' alt='temps'> Meteo`;
-      })
-    )
-    .catch((err) => console.log('Erreur : ' + err));
+  .then((response) =>
+    response.json().then((data) => {
+      console.log(data);
+      // On récupère la ville
+      document.querySelector('#ville').innerHTML = data.name;
+      // On récupère la température
+      document.querySelector('#temperature').innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp + '°';
+      // On récupère le taux d'humidité
+      document.querySelector('#humidite').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
+      // On récupère le % de vent
+      document.querySelector('#vent').innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed + 'km/h';
+      // On affiche l'image en fonction de la météo
+      document.querySelector('footer').innerHTML = `<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png' alt='temps'> Meteo`;
+    })
+  )
+    // Si il y a une erreur une renvoie le message d'erreur
+  .catch((err) => console.log('Erreur : ' + err));
 };
+  
 
-/*On récupère ce que l'utilisateur recherche*/
+// On récupère ce que l'utilisateur recherche
 document.querySelector('form').addEventListener('submit', function (e) {
   e.preventDefault();
   let ville = document.querySelector('#inputCity').value;
@@ -32,5 +33,5 @@ document.querySelector('form').addEventListener('submit', function (e) {
   apiCall(ville);
 });
 
-//Appel par defaut au chargement de la page
+// Appel par defaut au chargement de la page
 apiCall('Nantes');
